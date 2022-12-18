eea2b9d24c876738995871019fbf05306fd80205
vinnieb <vbalalian@gmail.com>
2022-12-18 12:23:35 -0800

    overwrite github mistake

--- coin.py
+++ coin.py
@@ -1,1 +1,41 @@
-#testing git again
+import time
+import random
+
+class coin(object):
+    def __init__(self):
+        self.sides = ['Heads', 'Tails']
+        self.side = random.choice(self.sides)
+        self.history = []
+    def flip(self):
+        self.side = random.choice(self.sides)
+        self.history.append(self.side)
+    def toss(self):
+
+        
+    def getHistory(self):
+        return self.history
+    def __str__(self):
+        return self.side
+
+class call(object):
+    def __init__(self):
+        self.history=[]
+        self.call=self.headsOrTails()
+    def headsOrTails(self):
+        try:
+            userCall=str(input('Heads or Tails? Call it: ')).title()
+            assert userCall=='Heads' or userCall=='Tails'
+            self.history.append(userCall)
+            return userCall
+        except:
+            print(f"'{userCall}' is not a valid call. Please enter 'Heads' "\
+                  + "or 'Tails'")
+            return self.headsOrTails()
+    def getCall(self):
+        if len(self.history) >= 1:
+            return "You called " + "\"" + str(self.call) + "\""
+    def getHistory(self):
+        return self.history   
+    def __str__(self):
+        return self.call
+
---
+++ cointoss.py
@@ -0,0 +1,14 @@
+from time import *
+from tkinter import *
+
+root = Tk()
+
+title = Label(root, text = 'Welcome to Cointoss!')
+subtitle = Label(root, text = 'Heads or Tails?')
+
+
+title.grid(row=0, column=1)
+
+
+
+root.mainloop()
